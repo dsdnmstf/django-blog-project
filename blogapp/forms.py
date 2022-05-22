5e7d2ea64db1cfb7874c67cf58410dc9e6cbd7eb
musrafa <dsdnmstf@gmail.com>
2022-05-22 19:45:18 +0200

    post_created added

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -3,15 +3,15 @@ from django import forms
 from.models import Category, Post, Comment
 
 class PostForm(forms.ModelForm):
-    status = forms.CharField(choices = Post.OPTIONS)
+    status = forms.ChoiceField(choices = Post.OPTIONS)
     category = forms.ModelChoiceField(
         queryset=Category.objects.all(), empty_label="Select")
     class Meta:
         model = Post
         fields = (
             "title",
-            "image",
             "content",
+            "image",
             "category",
             "status",
         )
@@ -19,4 +19,4 @@ class PostForm(forms.ModelForm):
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ("content")
+        fields = ("content",)
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(status="pb")
     context =  {
         "posts" : posts
     }
     return render(request, "blogapp/post_list.html", context)
+
+def post_create(request):
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)    
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("blogapp:list")
+
+    context = {
+        "form": form
+    }
+    return render(request, "blogapp/post_create.html", context )
+    
